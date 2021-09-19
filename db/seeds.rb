1b36f3241486132c17b9216092f6558ab0d00f33
Mike Esposito <mikeesposito6852@gmail.com>
2021-09-18 19:02:29 -0700

    Generate and seed Airport model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+codes = Airport.create([{ code: 'BUR' },
+                        { code: 'LAX' },
+                        { code: 'BED' },
+                        { code: 'BOS' },
+                        { code: 'ORD' },
+                        { code: 'MDW' }])
