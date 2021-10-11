dafaaea3d73094d13f301ce921de6d4c583b76e1
Mike Esposito <mikeesposito6852@gmail.com>
2021-10-10 19:32:35 -0700

    Add index to Flights Controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,7 @@
 class FlightsController < ApplicationController
   def index
+    @airport_options = Airport.all.map { |f| [ f.code, f.id ] }
+    @passenger_options = [ 1, 2, 3, 4 ]
+    @flight = Flight.new
   end
 end
