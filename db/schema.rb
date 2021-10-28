17deacae4af86f0a35e2cf64ef80d7a017eb650c
Mike Esposito <mikeesposito6852@gmail.com>
2021-10-28 07:32:33 -0700

    Add departure_time column to Flight

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_165227) do
+ActiveRecord::Schema.define(version: 2021_10_28_134055) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_10_26_165227) do
     t.integer "arrival_airport_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.time "departure_time"
   end
 
 end
