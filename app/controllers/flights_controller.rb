9d6d1420f4922cd5dbc5bba37735158df5f6d86f
Mike Esposito <mikeesposito6852@gmail.com>
2021-10-30 06:32:16 -0700

    Rename controller action

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,16 +5,17 @@ class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |f| [f.code, f.id] }
     @passenger_options = [1, 2, 3, 4]
-    @found_flights = found_flights
+    @found_flights = find_flights
   end
 
-  def found_flights
+  def find_flights
     return nil if params[:flight].nil?
-
+    
     Flight.find_flights(search_criteria)
   end
 
   def search_criteria
+    Rails.logger.debug "CONTROLLER SEARCH CRITERIA PARAMS: #{params}}"
     { departure_airport_id: params[:flight][:departure_airport_id],
       arrival_airport_id: params[:flight][:arrival_airport_id],
       departure_date: params[:flight][:departure_date] }
