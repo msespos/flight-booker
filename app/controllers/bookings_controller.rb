d0d524a4c2dd49d2747ef050a1ec0f846ae5a851
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-02 11:23:03 -0700

    Re-add @booking = Booking.new and add empty create method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,9 +3,12 @@
 # Bookings Controller class
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
     @flight_id = params[:flight][:id]
     @number_of_passengers = params[:flight][:number_of_passengers]
     @flight = Flight.find(@flight_id)
     @passengers = Booking.passengers(@number_of_passengers)
   end
+
+  def create; end
 end
