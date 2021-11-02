bfd67fb22527d1379a5086121caa23e106c70b18
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-02 11:23:33 -0700

    Remove unnecessary variable assignment

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
+# Booking class
 class Booking < ApplicationRecord
   belongs_to :flight
   has_many :passengers
 
   def self.passengers(number_of_passengers)
     passengers = []
-    number_of_passengers.to_i.times do |passenger_number|
+    number_of_passengers.to_i.times do
       passengers << Passenger.new
     end
     passengers
