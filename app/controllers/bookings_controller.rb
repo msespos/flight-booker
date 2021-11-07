3cd9305b1ac72ab0b549ff53dbff05f43198ca56
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-06 17:09:07 -0700

    Add create and show and extract_booking_params! methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,8 +7,28 @@ class BookingsController < ApplicationController
     @flight_id = params[:flight][:id]
     @number_of_passengers = params[:flight][:number_of_passengers]
     @flight = Flight.find(@flight_id)
-    @passengers = Booking.passengers(@number_of_passengers)
+    @passengers = Booking.create_passengers(@number_of_passengers)
   end
 
-  def create; end
+  def create
+    @booking = Booking.new(extract_booking_params!)
+    if @booking.save
+      redirect_to @booking
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+    def extract_booking_params!
+      params[:booking][:passengers_attributes] = params[:booking][:passenger]
+      params[:booking].delete(:passenger)
+      params.require(:booking).permit(:flight_id,
+                                      passengers_attributes: [:name, :email])
+    end
 end
