d57aac07d657a50396ebea55251ebdf68c6a6b80
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-12 09:04:00 -0800

    Appease Rubocop and edit flash message

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/MethodLength
+
 # Bookings Controller class
 class BookingsController < ApplicationController
   def new
     if params[:flight][:id].nil?
-      flash[:alert] = 'Please redo your search and make a selection before booking.'
+      flash[:alert] = 'Please make a flight selection.'
       redirect_to controller: :flights, action: :index,
                   number_of_passengers: params[:number_of_passengers],
                   flight: {
                     departure_airport_id: params[:flight][:departure_airport_id],
                     arrival_airport_id: params[:flight][:arrival_airport_id],
-                    departure_date: params[:flight][:departure_date] }
+                    departure_date: params[:flight][:departure_date]
+                  }
     else
       @booking = Booking.new
       @number_of_passengers = params[:number_of_passengers]
@@ -42,3 +46,6 @@ class BookingsController < ApplicationController
                                     passengers_attributes: %i[name email])
   end
 end
+
+# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/MethodLength
