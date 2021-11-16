4b1effb103839cd5a21a4b05d3e8a563c90a3c8f
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-16 08:49:21 -0800

    Add/edit tests for #create_passengers

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -6,13 +6,37 @@ describe Booking do
   describe '#create_passengers' do
     context 'when passed 0' do
       it 'returns an empty array' do
-        expect(Booking.create_passengers(0)).to eq([])
+        booking = Booking.create_passengers(0)
+        expect(booking).to eq([])
       end
     end
     context 'when passed 1' do
       it 'returns an array with one passenger' do
-        passenger = Passenger.new
-        expect(Booking.create_passengers(1)).to eq([passenger])
+        booking = Booking.create_passengers(1)
+        expect(booking.length).to eq(1)
+      end
+    end
+    context 'when passed 1' do
+      it 'returns an array with one passenger with the correct attributes' do
+        booking = Booking.create_passengers(1)
+        expect(booking[0]).to have_attributes(id: nil, name: nil, email: nil,
+                                              booking_id: nil, created_at: nil,
+                                              updated_at: nil)
+      end
+    end
+    context 'when passed 4' do
+      it 'returns an array with four passengers' do
+        booking = Booking.create_passengers(4)
+        expect(booking.length).to eq(4)
+      end
+    end
+    context 'when passed 4' do
+      it 'returns an array with with the correct attributes for the fourth
+          passenger' do
+        booking = Booking.create_passengers(4)
+        expect(booking[3]).to have_attributes(id: nil, name: nil, email: nil,
+                                              booking_id: nil, created_at: nil,
+                                              updated_at: nil)
       end
     end
   end
