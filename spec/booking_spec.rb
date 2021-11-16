36e9faf65af2fbbda5ae07860cc39aecd45c872c
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-16 09:04:11 -0800

    Appease Rubocop

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
+
 require 'rails_helper'
 
 describe Booking do
@@ -41,3 +43,5 @@ describe Booking do
     end
   end
 end
+
+# rubocop:enable Metrics/BlockLength
