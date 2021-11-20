ff87d87c1f5a924cfb8e4c1807b462c6c0a6d10d
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-19 17:04:41 -0800

    Edit flight factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,15 +1,7 @@
 FactoryBot.define do
   factory :flight do
-    trait :departing_from_BUR do
-      departure_airport_id { 1 }
-    end
-
-    trait :arriving_at_LAX do
-      arrival_airport_id { 2 }
-    end
-
-    trait :departing_nov_1_2021 do
-      departure_date { '01/11/2021' }
-    end
+    departure_airport_id { 1 }
+    arrival_airport_id { 2 }
+    departure_date { '01/11/2021' }
   end
 end
