d768d629324cd14bca329fef0c383739bd4f5274
Mike Esposito <mikeesposito6852@gmail.com>
2021-11-20 17:28:05 -0800

    add first feature tests

---
+++ spec/features/first_search_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Perform the first search', type: :feature do
+  scenario 'with defaults' do
+    visit root_path
+    click_on 'Search!'
+    expect(page).to have_content('No flights found')
+  end
+
+  scenario 'with defaults except arrival airport LAX' do
+    visit root_path
+    select 'LAX', from: 'flight_arrival_airport_id'
+    click_on 'Search!'
+    expect(page).to have_content('BUR LAX 01/11/2021')
+  end
+end
