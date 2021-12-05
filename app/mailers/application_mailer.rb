afb9fb1c8b3b798d1e92254719509e081cf8cea2
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-05 14:43:41 -0800

    Add confirmation_email method

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,4 +3,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def confirmation_email
+    @passenger = params[:passenger]
+    @url  = 'http://example.com/login'
+    mail(to: @passenger.email, subject: 'Thank you for booking!')
+  end
 end
