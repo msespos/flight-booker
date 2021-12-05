70ff47cd2160fa67ef19f59ea19b14b8979772f5
Mike Esposito <mikeesposito6852@gmail.com>
2021-12-05 15:57:36 -0800

    Configure letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,9 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   config.reload_classes_only_on_change = false
 
   # In the development environment your application's code is reloaded any time
